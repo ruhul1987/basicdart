abbe3ce83facb266cb692555fbd0e2020cc46bc3
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 10:19:11 +0300

    Create list.dart

---
+++ list.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<String> names = [
+    "Ruhul Amin",
+    "Sabina Yasmin",
+    "Rafia Tasnim ",
+    "Rukaiya Tasfia"
+  ];
+
+  print("Value of Names [0] is : ${names[0]}");
+  print("Value of Names [1] is : ${names[1]}");
+  print("Value of Names [2] is : ${names[2]}");
+  print("Value of Names [3] is : ${names[3]}");
+
+  int length = names.length;
+  print("The length of Names is $length");
+}
