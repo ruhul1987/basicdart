30780ef9211a1ad2b9edd201bb6cd4f711996538
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 10:09:26 +0300

    Create intToString.dart

---
+++ intToString.dart
@@ -0,0 +1,10 @@
+void main() {
+  int one = 1;
+  print(one);
+  print("Type of one is : ${one.runtimeType}");
+
+  String convert = one.toString();
+  print(convert);
+
+  print("Type of one is : ${convert.runtimeType}");
+}
