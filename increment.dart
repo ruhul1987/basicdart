2e58fa96057371a91615476136913d4f3b2b7f8a
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 13:08:01 +0300

    Create increment.dart

---
+++ increment.dart
@@ -0,0 +1,15 @@
+void main() {
+  int num1 = 0;
+  int num2 = 0;
+
+  //Pre increment
+  num2 = ++num1;
+  print("The value of num2 is $num2");
+
+  num2 = 0;
+  num1 = 0;
+
+  //num1 = num2++;
+  num2 = num1++;
+  print("The value of num2 is $num2");
+}
