61897ecbd6826acbc8d15ac143ef8f692dcb29cd
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 10:36:54 +0300

    ok

---
+++ map.dart
@@ -0,0 +1,10 @@
+void main() {
+  Map<String, String> myDetails = {
+    'name': 'Ruhul Amin',
+    'address': 'Thakurgaon',
+    'country': 'BD'
+  };
+
+  print("My Name is ${myDetails['name']}");
+  print("My Country is ${myDetails['country']}");
+}
---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,1 @@
+myDetails['country']
