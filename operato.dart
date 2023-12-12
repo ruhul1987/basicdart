ec1e635636e71c857b2efedbec40d13b790533aa
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 13:02:04 +0300

    Create operato.dart

---
+++ operato.dart
@@ -0,0 +1,22 @@
+void main() {
+  int num1 = 10;
+  int num2 = 2;
+
+  // Performing arithmetic calculation
+
+  int sum = num1 + num2; //addition
+  int diff = num1 - num2; //Subtraction
+  int unaryMinus = -num1; //unary minus
+  int mul = num1 * num2; //multiplication
+  int div = num1 ~/ num2; // integer division
+  double div2 = num1 / num2; //division
+  int mod = num1 % num2; //Remainder
+
+  print("This addition is $sum");
+  print("This subtraction is $diff");
+  print("This unary minus is $unaryMinus");
+  print("This multiplication is $mul");
+  print("This integer division is $div");
+  print("This division is $div2");
+  print("This remainder is $mod");
+}
