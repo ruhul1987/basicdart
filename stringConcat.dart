29d69af318d56195e561ade450a5a0dd2d9c6361
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 14:13:26 +0300

    Create stringConcat.dart

---
+++ stringConcat.dart
@@ -0,0 +1,7 @@
+void main() {
+  String firstName = "John";
+  String lastName = "Doe";
+
+  print("Using +, Full name is " + firstName + " " + lastName + ".");
+  print("Using interpolation Full name is $firstName $lastName.");
+}
