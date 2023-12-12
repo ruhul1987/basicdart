0a04b44606fdb9f3ae125f35db0f6659fb87c6c9
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 14:09:00 +0300

    Create string1.dart

---
+++ string1.dart
@@ -0,0 +1,10 @@
+void main() {
+  String text1 = 'This is an example of a single line string.';
+  String text2 = "This is an example\'s single line string.";
+  String text3 = '''This is an example of a 
+  multi line string.''';
+
+  print(text1);
+  print(text2);
+  print(text3);
+}
