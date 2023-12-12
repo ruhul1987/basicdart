d2af752932354960bc6c5d01706dfa1648a1fd99
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 09:25:42 +0300

    Create multilineString.dart

---
+++ multilineString.dart
@@ -0,0 +1,15 @@
+void main() {
+  String multilLine = '''
+  This is Multi Line Text
+  with 3 single quote
+  i am also writing here.
+
+  ''';
+  String anotherMultiLine = """
+This is another MultiLine 
+Text with three double quote
+I am also type here.""";
+
+  print("Single Three Quote $multilLine");
+  print("Double Three Quote $anotherMultiLine");
+}
