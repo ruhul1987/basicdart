0765e077236cb238b135b12dce774419df43ce03
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-12 09:11:54 +0300

    Create number.dart

---
+++ number.dart
@@ -0,0 +1,14 @@
+void main() {
+  int num1 = 10;
+  double num2 = 50.50;
+  num num3 = 100;
+  num num4 = 150.50;
+
+  num sum = num1 + num2 + num3 + num4;
+
+  print("Number one : $num1");
+  print("Number Two : $num2");
+  print("Number Three : $num3");
+  print("Number four : $num4");
+  print("Sum of all number: $sum");
+}
