b114df7ed6fe414379eea6613601e0a6ae21737c
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-13 08:09:43 +0300

    Create methodOfString.dart

---
+++ methodOfString.dart
@@ -0,0 +1,7 @@
+void main() {
+  String address1 = "Florida";
+  String address2 = "Dhaka";
+
+  print("Address 1 in upper Case:${address1.toUpperCase()}");
+  print("Address 2 is lower case: ${address2.toLowerCase()}");
+}
