31e397d396b50d6e016ede3782e59854d54dd3ec
Ruhul Amin <okp10.wksp@gmail.com>
2023-12-13 12:40:19 +0300

    Update pq4.dart

---
+++ pq4.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+  //int num1 = 10;
+  print("Enter First Number: ");
+  int num1 = int.parse(stdin.readLineSync()!);
+  //int num2 = 20;
+  print("Enter Second Number: ");
+  int num2 = int.parse(stdin.readLineSync()!);*
+
+  // Get input from user
+  int temp = num1;
+  num1 = num2;
+  num2 = temp;
+
+  print("After swapping: ");
+  print("Frist Number is $num1");
+  print("Second Number is $num2");
+}
